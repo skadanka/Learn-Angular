11afcf6136729aec7a38ffb0ec15063211dacd4a
skadanka <rylynn2@gmail.com>
2023-05-12 09:17:25 +0300

    setup of firebase in angular project

--- Angular-course/project-fullstack/firestarter-demo/src/app/app.module.ts
+++ Angular-course/project-fullstack/firestarter-demo/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
+import { HomePageComponent } from './home-page/home-page.component';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
